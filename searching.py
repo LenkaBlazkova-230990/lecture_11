ee60c51344fd8bb9ebe0c2a1918c645374853a23
Lenka Blazkova <xblazk01@vutbr.cz>
2021-04-19 10:32:34 +0200

    Linear search function

--- searching.py
+++ searching.py
@@ -26,10 +26,37 @@ def read_data(file_name, field):
     return sequence
 
 
+def linear_search(sequential_data, searched_number):
+    """
+    Linear searching in unordered list
+
+    :param sequential_data: (list) list of input numbers
+    :param searched_number: (int) searched number
+    :return: dictionary - positions of searched number and its count
+    """
+    indices_position = []
+    count = 0
+
+    for position, number in enumerate(sequential_data):
+        if number == searched_number:
+            indices_position.append(position)
+            count += 1
+
+    dict = {
+        "positions": indices_position,
+        "count": count
+    }
+
+    return dict
+
+
 def main():
     file_name = "sequential.json"
     sequential_data = read_data(file_name, "unordered_numbers")
-    print(sequential_data)
+    # print(sequential_data)
+
+    linear_search_results = linear_search(sequential_data, searched_number=0)
+    print(linear_search_results)
     # pass
 
 

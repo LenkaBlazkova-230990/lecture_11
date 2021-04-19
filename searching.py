144d6b32a3776945be1a1faabe383ac681bffaa3
Lenka Blazkova <xblazk01@vutbr.cz>
2021-04-19 10:02:40 +0200

    Read data function

--- searching.py
+++ searching.py
@@ -1,4 +1,6 @@
 import os
+import json
+
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -7,14 +9,31 @@ cwd_path = os.getcwd()
 def read_data(file_name, field):
     """
     Reads json file and returns sequential data.
+
     :param file_name: (str), name of json file
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    if field not in {"unordered_numbers", "ordered_numberes", "dna_sequence"}:
+        return None
+
+    with open(file_path, 'r') as json_file:
+        dictionary = json.load(json_file)
+
+    sequence = dictionary[field]
+    return sequence
+
+
+
+
+
 
 def main():
+    file_name = "sequential.json"
+    sequential_data = read_data(file_name, "unordered_numbers")
+    print(sequential_data)
     pass
 
 
